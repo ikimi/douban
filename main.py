5bc62c39ed50ec0dacb50e6718ed05000195c95d
icys13 <jiajun.li13@gmail.com>
2011-11-29 23:27:52 +0800

    多线程遍历文件名  ----11/29/2011 23:28 udonmai_.... 文件名 改为 main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import allfiles
+import update
+import queue
+from Queue import Queue
+
+if __name__=="__main__":
+	path="/home/icys/桌面/wxPython/douban/奶茶"
+	#目录下所有mp3文件列表
+	list = allfiles.file_list(path)
+	mnames = list.listout(0)
+	# mnames 元素个数
+	num = len(mnames)
+	#print num
+	# 设置 queue 最多同时可以执行10个线程
+	que = Queue(maxsize = 10)
+	#print queue.maxsize
+	# 建立producer子线程
+	# 第二个 num 为 mnames 中mp3文件个数
+	pro = queue.producer(que,num,mnames)
+	pro.start()
+	pro.join
+	#for name in mnames:
+	#	music = update.update(name)
+	#	music.search()
+
+
+
+
--- queue.py
+++ queue.py
@@ -1,42 +1,55 @@
 # -*- coding: utf-8 -*-
 import threading
 import time
+import update
 from Queue import Queue
 
+# -*- 类接受两个参数 
+# -*- name 在信号量队列中的编号
+# -*- queue 信号量队列
+
 class customer(threading.Thread):
-	def __init__(self,t_name,queue):
-		threading.Thread.__init__(self,name=t_name)
-		self.data=queue
-		self.name=t_name
+	Data = []
+	def __init__(self,t_name,queue,data):
+		threading.Thread.__init__(self,name =t_name)
+		self.data = queue
+		self.name = t_name
+		self.Data = data
 	def run(self):
-		time.sleep(6)
+		time.sleep(14)
+	#	update.update()
 		print self.name
+		print self.Data[0] + '/' +self.Data[1] + '.mp3'
 		self.data.get()
 
+# -*- 接受两个参数	
+# -*- queue 信号量队列	
+# -*- num 总的信息数量
+
 class producer(threading.Thread):
-	def __init__(self,queue,num):
+	mnames = []
+	def __init__(self,queue,num,mnames):
 		threading.Thread.__init__(self)
 		self.num = num
 		self.data = queue
-	#	print self.data.maxsize	
+		self.mnames = mnames
 	def run(self):
 		i = 0
 		while i<self.num :
-			n = i%5
+			n = i%self.data.maxsize
 			self.data.put(n)
 			#创建一个新进程
-			c = customer(n,self.data)
+			c = customer(n,self.data,self.mnames[i])
 			c.start()
 			i+=1
 			time.sleep(1)
 					
 
-def main():
-	queue = Queue(maxsize=5)
-#	print queue.maxsize
-	p = producer(queue,10)
-	p.start()
-	p.join()
+#def main():
+#	queue = Queue(maxsize=5)
+#	p = producer(queue,10)
+#	p.start()
+#	p.join()
 
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+#	main()
