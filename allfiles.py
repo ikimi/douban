63fba508e5d28e15da7ee36f02d85a8afa2d9d73
udonmai <udonmai@gmail.com>
2011-12-01 16:02:46 +0800

    只匹配mp3

--- allfiles.py
+++ allfiles.py
@@ -10,7 +10,7 @@ class file_list:
 	
 	def listout(self, start, end = None):
 		i = start 
-		filvename = ''
+		filename = ''
 		path_list = []
 		flag = True#标识是否有结束位置的设置
 		if end == None:
@@ -18,6 +18,9 @@ class file_list:
 
 		for root,dirs,files in os.walk(self.path):
 			for filespath in files:
+				#print filespath
+				if not filespath.endswith('mp3'):
+					break
 				filename = filespath.partition('.mp3')#分片
 				path_list.append([root,filename[0]])
 				i += 1
